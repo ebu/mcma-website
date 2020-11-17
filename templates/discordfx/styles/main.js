199f1ed3f6a68503c155d67e85dd5177c2bb8df3
Evan Verney-Fink <evan@triskel-inc.com>
2020-11-17 11:10:12 -0500

    Ongoing website work

--- templates/discordfx/styles/main.js
+++ templates/discordfx/styles/main.js
@@ -4,17 +4,26 @@ function toggleMenu() {
                
     var x = document.getElementById("sidebar");
     var b = document.getElementById("blackout");
+    
+    const sidebarHeader = document.getElementById("sidebar-header");
+    const sidebarHeaderSeparator = document.getElementById("sidebar-header-separator");
 
     if (x.style.left === "0px") 
     {
-        x.style.left = "-350px";
+        x.style.left = "calc(var(--sidebar-width) * -1)";
         b.classList.remove("showThat");
         b.classList.add("hideThat");
+        
+        sidebarHeader.classList.remove("hide");
+        sidebarHeaderSeparator.classList.remove("hide");
     } 
     else 
     {
         x.style.left = "0px";
         b.classList.remove("hideThat");
         b.classList.add("showThat");
+        
+        sidebarHeader.classList.add("hide");
+        sidebarHeaderSeparator.classList.add("hide");
     }
 }
